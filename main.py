f327f561e3f7d75979bce372a9883573ef998fa9
Ana Ban <anabananaslo@hotmail.com>
2020-03-04 19:04:48 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+txt = "Today Is A BeautiFul DAY!"
+
+x = txt.capitalize()
+
+print (x)
+
+
+str_one ="Happy"
+str_two ="Day"
+
+print(str_one + " " + str_two)
+print("%s %s" %(str_one, str_two))
+print("{0} {1}".format(str_one, str_two))
